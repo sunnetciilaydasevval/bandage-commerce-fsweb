32204bb3dd3ecea7af1df3416128106b8e595fc5
İlayda Şevval Sünnetçi <sunnetci.ilaydasevval@gmail.com>
2026-08-21 20:08:05 +0300

    Initial commit

---
+++ frontend/eslint.config.js
@@ -0,0 +1,39 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
+  globalIgnores(['dist', 'node_modules']),
+  {
+    files: ['**/*.{js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+      },
+      parserOptions: {
+        ecmaFeatures: { jsx: true }
+      },
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+])
---
+++ frontend/src/i18n/i18n.js
@@ -0,0 +1,27 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+import trCommon from './locales/tr/common.json';
+import enCommon from './locales/en/common.json';
+
+i18n
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        resources: {
+            tr: {
+                common: trCommon
+            },
+            en: {
+                common: enCommon
+            }
+        },
+        fallbackLng: 'en',
+        ns: ['common'],
+        defaultNS: 'common',
+        interpolation: {
+            escapeValue: false
+        }
+    });
+
+export default i18n;
